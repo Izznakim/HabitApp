af183753d1616f4e622949d65a78513c6b58ead0
Izznakim <IzzuddinNaufalHakim@gmail.com>
2022-06-30 06:52:37 +0700

    TODO 11 SOLVED.

--- app/src/main/java/com/dicoding/habitapp/setting/SettingsActivity.kt
+++ app/src/main/java/com/dicoding/habitapp/setting/SettingsActivity.kt
@@ -1,10 +1,13 @@
 package com.dicoding.habitapp.setting
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 import com.dicoding.habitapp.R
+import com.dicoding.habitapp.utils.DarkMode
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -20,17 +23,41 @@ class SettingsActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    class SettingsFragment : PreferenceFragmentCompat() {
+    class SettingsFragment : PreferenceFragmentCompat(),
+        SharedPreferences.OnSharedPreferenceChangeListener {
+        private lateinit var THEME:String
+
+        private lateinit var themePreference:ListPreference
+
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
-            //TODO 11 : Update theme based on value in ListPreference
+            //TODO 11 : Update theme based on value in ListPreference [SOLVED]
+            THEME=getString(R.string.pref_key_dark)
+            themePreference = findPreference<ListPreference>(THEME) as ListPreference
+        }
+
+        override fun onResume() {
+            super.onResume()
+            preferenceScreen.sharedPreferences?.registerOnSharedPreferenceChangeListener(this)
+        }
+
+        override fun onPause() {
+            super.onPause()
+            preferenceScreen.sharedPreferences?.unregisterOnSharedPreferenceChangeListener(this)
         }
 
         private fun updateTheme(mode: Int): Boolean {
             AppCompatDelegate.setDefaultNightMode(mode)
             requireActivity().recreate()
             return true
+        }
+
+        override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
+            if (key == THEME) {
+                val mode = DarkMode.valueOf(themePreference.value.toUpperCase())
+                updateTheme(mode.value)
+            }
         }
     }
 }
